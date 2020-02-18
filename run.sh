b057a2c4f8ddd86b6afdd0fef7b39dee6603a308
eastonYi <yicheng1994@outlook.com>
2020-02-18 22:59:06 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 export BERT_BASE_DIR=/data3/easton/data/pretrain/chinese_L-12_H-768_A-12
-CUDA_VISIBLE_DEVICES= 
+CUDA_VISIBLE_DEVICES=
 python main.py \
   --input_file=test.zh.tsv \
   --vocab_file=vocab.txt \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
   --max_seq_length=128 \
-  --output_dir=/tmp/lm_output/
-  # --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --output_dir=.
