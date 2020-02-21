8d94a591fd5702ca6f1d60ba027f90b6871c9358
eastonYi <yicheng1994@outlook.com>
2020-02-21 23:52:21 +0800

    local change

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 export BERT_BASE_DIR=/data3/easton/data/pretrain/chinese_L-12_H-768_A-12
 CUDA_VISIBLE_DEVICES= python main.py \
-  --input_file=test.zh.tsv \
+  --input_file=preds_of_beam1_from_average8ckpts-0.probs \
   --vocab_file=vocab.txt \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
