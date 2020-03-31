95e7921722b27dd8f9902f17a28b55da18422c2a
eastonYi <yicheng1994@outlook.com>
2020-03-31 15:05:11 +0800

    local change

--- data_reader.py
+++ data_reader.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 import tokenization
-import queue
 import re
 
 MASKED_TOKEN = "[MASK]"
@@ -12,13 +11,10 @@ class TextDataSet():
     """
     def __init__(self, data_file, vocab_file, max_seq_length):
         self.data_file = data_file
-        # self.token2idx, self.idx2token = args.token2idx, args.idx2token
         self.size_dataset = self.get_size()
         self.max_seq_length = max_seq_length
         self.tokenizer = tokenization.FullTokenizer(vocab_file=vocab_file, do_lower_case=True)
         self.MASKED_ID = self.tokenizer.convert_tokens_to_ids([MASKED_TOKEN])[0]
-        self.queue_tokens = queue.Queue(20000)
-        self.queue_uttids = queue.Queue(2000)
 
     def __len__(self):
         return self.size_dataset
@@ -33,42 +29,46 @@ class TextDataSet():
                 uttid, text = line.strip().split()
                 tokens = self.tokenizer.tokenize(text)
 
-                # Account for [CLS] and [SEP] with "- 2"
-                if len(tokens) > self.max_seq_length - 2:
-                    tokens = tokens[0:(self.max_seq_length - 2)]
+                try:
+                    assert len(text) <= self.max_seq_length - 2
+                except AssertionError:
+                    print(text, '. too long')
+                    continue
 
-                input_tokens = ["[CLS]"] + tokens + ["[SEP]"]
-                len_pad = self.max_seq_length - len(input_tokens)
-                input_ids = self.tokenizer.convert_tokens_to_ids(input_tokens) + [0] * len_pad
-                input_mask = [1] * len(input_tokens) + [0] * len_pad
-
-                [self.queue_tokens.put(i) for i in input_tokens]
-                self.queue_uttids.put(uttid)
+                try:
+                    input_tokens = ["[CLS]"] + tokens + ["[SEP]"]
+                    len_pad = self.max_seq_length - len(input_tokens)
+                    input_ids = self.tokenizer.convert_tokens_to_ids(input_tokens) + [0] * len_pad
+                    input_mask = [1] * len(input_tokens) + [0] * len_pad
+                except KeyError:
+                    print(text, '. OOV')
+                    continue
 
                 if i % 1000 == 0:
                     print('processed {} sentences.'.format(i))
 
-                yield from self.create_sequential_mask(input_tokens, input_ids, input_mask)
+                yield uttid, tokens, self.create_sequential_mask(input_tokens, input_ids, input_mask)
 
     def create_sequential_mask(self, input_tokens, input_ids, input_mask):
         """Mask each token/word sequentially"""
 
+        list_inputs = []
         for i in range(1, len(input_tokens)-1):
             mask_count = 1
             while is_subtoken(input_tokens[i+mask_count]):
                 mask_count += 1
 
-            input_ids_new, masked_lm_positions, masked_lm_labels = \
+            input_ids_new, masked_lm_positions = \
                 self.create_masked_lm_prediction(input_ids, i, mask_count)
             pad_len = self.max_seq_length - len(masked_lm_positions)
 
             masked_lm_positions += [0] * pad_len
-            masked_lm_labels += [0] * pad_len
 
             i += mask_count
-            output = (input_ids_new, input_mask, masked_lm_positions, masked_lm_labels)
+            output = (input_ids_new, input_mask, masked_lm_positions)
+            list_inputs.append(output)
 
-            yield output
+        return list_inputs
 
     def create_masked_lm_prediction(self, input_ids, mask_position, mask_count=1):
         new_input_ids = list(input_ids)
@@ -78,7 +78,8 @@ class TextDataSet():
             new_input_ids[i] = self.MASKED_ID
             candidate_lm_labels.append(input_ids[i])
 
-        return new_input_ids, masked_lm_positions, candidate_lm_labels
+        return new_input_ids, masked_lm_positions
+
 
 
 def is_subtoken(x):
@@ -97,18 +98,8 @@ class ASRDecoded(TextDataSet):
                 _, ref = line_ref.strip().split()
                 _, text, candidates = line.strip().split(',', maxsplit=2)
 
-                # filter samples
-                try:
-                    assert re.findall("[\u4e00-\u9fa5]+", text)[0] == text
-                    assert len(self.tokenizer.tokenize(ref)) == len(ref)
-                except (IndexError, AssertionError):
-                    fw.write(ref + ', ' + text + ' subword' + '\n')
-                    continue
-
-                try:
-                    assert len(text) <= self.max_seq_length - 2
-                except AssertionError:
-                    fw.write(ref + ', ' + text + ' too long\n')
+                # check if the sent is too long or contains OOV
+                if not self.check(text, ref, fw):
                     continue
 
                 tokens = list(text)
@@ -176,3 +167,113 @@ class ASRDecoded(TextDataSet):
             new_input_ids[i] = self.MASKED_ID
 
         return new_input_ids, masked_lm_positions
+
+    def check(self, text, ref, fw):
+        # filter samples
+        try:
+            assert re.findall("[\u4e00-\u9fa5]+", text)[0] == text
+            assert len(self.tokenizer.tokenize(ref)) == len(ref)
+        except (IndexError, AssertionError):
+            fw.write(ref + ', ' + text + ' subword' + '\n')
+            return 0
+
+        try:
+            assert len(text) <= self.max_seq_length - 2
+        except AssertionError:
+            fw.write(ref + ', ' + text + ' too long\n')
+            return 0
+
+        return 1
+
+
+class ASRDecoded2(ASRDecoded):
+    def __init__(self, data_file, vocab_file, max_seq_length):
+        super().__init__(data_file, None, vocab_file, max_seq_length)
+
+    def __iter__(self):
+        with open(self.data_file) as f, open('samples.droped', 'w') as fw:
+            num_converted = 0
+            for i, line in enumerate(f):
+                uttid, ref, res, candidates = \
+                    [i.split(':', maxsplit=1)[1] for i in line.strip().split(',', maxsplit=3)]
+
+                # filter samples
+                if not self.check(res, fw):
+                    continue
+
+                tokens = list(res)
+                list_all_cands = candidates.split()
+                assert len(list_all_cands) == len(tokens)
+
+                num_converted += 1
+
+                if i % 10000 == 0:
+                    print('processed {} sentences.'.format(i))
+
+                yield uttid, ref, res, list_all_cands
+
+        print('***************utilized {}/{} samples to be fake samples*********************'.format(num_converted, i+1))
+
+    def gen_input(self, list_decoded_cands, list_vague_idx):
+        assert list_vague_idx
+        list_res_new = [i[0].split(':')[0] for i in list_decoded_cands]
+        input_tokens = ["[CLS]"] + list_res_new + ["[SEP]"]
+        len_pad = self.max_seq_length - len(input_tokens)
+        input_ids = self.tokenizer.convert_tokens_to_ids(input_tokens) + [0] * len_pad
+        input_mask = [1] * len(input_tokens) + [0] * len_pad
+
+        return self.create_sequential_mask(input_ids, input_mask, list_vague_idx)
+
+    def check(self, text, fw):
+        # filter samples
+        try:
+            assert re.findall("[\u4e00-\u9fa5]+", text)[0] == text
+        except (IndexError, AssertionError):
+            fw.write(text + ' subword' + '\n')
+            return 0
+
+        try:
+            assert len(text) <= self.max_seq_length - 2
+        except AssertionError:
+            fw.write(text + ' too long\n')
+            return 0
+
+        return 1
+
+
+def cand_threshold(list_all_cands):
+    list_all_cands_new = []
+    list_vague_idx = []
+    for i, cands in enumerate(list_all_cands):
+        list_cands = []
+        for cand in cands.split(','):
+            if float(cand.split(':')[1]) > 0.0:
+                list_cands.append(cand)
+        if len(list_cands) == 0:
+            list_cands = cands.split(',')
+            list_vague_idx.append(i)
+        elif len(list_cands) == 1:
+            list_cands = list_cands[0].split(':')[0]
+        else:
+            list_vague_idx.append(i)
+        list_all_cands_new.append(list_cands)
+
+    return list_all_cands_new, list_vague_idx
+
+
+def choose(list_all_cands):
+    list_idx = []
+    anchors = []
+    list_to_fix = []
+    for i, x in enumerate(list_all_cands):
+        if len(x) > 1:
+            list_idx.append(i)
+        else:
+            anchors.append(i)
+    for idx in list_idx:
+        left_cond = (idx - 1 in anchors) and (idx - 2 in anchors)
+        right_cond = (idx + 1 in anchors) and (idx + 2 in anchors)
+        if left_cond or right_cond:
+            list_to_fix.append(idx)
+
+    return list_to_fix
--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 export BERT_BASE_DIR=/data3/easton/data/pretrain/chinese_L-12_H-768_A-12
 CUDA_VISIBLE_DEVICES=1 python main.py \
-  --input_file=16_beam100-of-900_from_average8ckpts-0.top4.res.part2 \
+  --input_file=16_beam100-of-900_from_average8ckpts-0.top4.res \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
   --max_seq_length=30 \
   --predict_batch_size=100 \
-  --output='16_beam100-of-900_from_average8ckpts-0.top4.res.lm-prob.part2'
+  --output='16_beam100-of-900_from_average8ckpts-0.top4.res.lm-prob'
