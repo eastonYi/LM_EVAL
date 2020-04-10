e43aa27a6781b7effc5b99aacb77db8766105b4c
eastonYi <yicheng1994@outlook.com>
2020-04-10 11:56:38 +0800

    fix

--- fix_cn.sh
+++ fix_cn.sh
@@ -1,9 +1,11 @@
+gpu=$1
 export BERT_BASE_DIR=/data3/easton/data/pretrain/chinese_L-12_H-768_A-12
-CUDA_VISIBLE_DEVICES=0 python main.py \
-  --input_file='inputs/600-of-2000_sos_beam-1.top4.probs' \
+CUDA_VISIBLE_DEVICES=$gpu python main.py \
+  --input_file='inputs/model-100h_beam-cand-decode_300h.cand' \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
   --max_seq_length=40 \
   --predict_batch_size=10 \
-  --output='outputs/600-of-2000_sos_beam-1.top4.trans'
+  --output='outputs/model-100h_beam-cand-decode_300h.cand' \
+  --is_cn
